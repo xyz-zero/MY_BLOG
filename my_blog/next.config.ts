d843ccd2f7020f3984e486cf25082eb3ae632c01
big-bamp <1528912119@qq.com>
2025-06-20 21:55:58 +0800

    feat: 基础配置

--- my_blog/next.config.ts
+++ my_blog/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
